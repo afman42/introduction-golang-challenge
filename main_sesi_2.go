578bb0a123a088500a72831f2d03f8bc4069a3e7
afman42 <afifarman50@gmail.com>
2023-03-10 14:49:26 +0700

    add comment file main sesi 2

--- main_sesi_2.go
+++ main_sesi_2.go
@@ -3,22 +3,31 @@ package main
 import "fmt"
 
 func main() {
-	var iFor int = 10
-	var character string = "CA日APBO"
-	// for
+	var (
+		iFor int    = 10
+		word string = "CA日APBO"
+	)
+
+	// for looping Nilai i
 	for i := 0; i < iFor; i++ {
 		if i < 5 {
 			fmt.Println("Nilai i = ", i)
 		}
 	}
+
+	// for looping Nilai j
 	for j := 0; j < iFor; j++ {
 		if j < 5 {
 			fmt.Println("Nilai j = ", j)
 		}
 	}
-	for index, word := range character {
-		fmt.Printf("character %#U starts at byte position %d\n", word, index)
+
+	// for looping character
+	for index, alphabet := range word {
+		fmt.Printf("character %#U starts at byte position %d\n", alphabet, index)
 	}
+
+	// for looping Nilai j
 	for j := 6; j <= iFor; j++ {
 		if j <= 10 {
 			fmt.Println("Nilai j = ", j)
